210b56a24a93a3f91f7483220477e0380a95ebb7
root <root@howe-himem-assembly.novalocal>
2015-02-26 22:41:45 +0000

    added scripts

--- get-ncbi-id2.py
+++ get-ncbi-id2.py
@@ -1,7 +1,7 @@
 import sys, screed
 
 for record in screed.open(sys.argv[1]):
-    if record.name.startswith("NZ_"):
+    if len(record.name.split('_')[0]) < 5:
         print record.name.split('_')[0] + "_" + record.name.split('_')[1]
     else:
         print record.name.split('_')[0]
--- to-get-taxa.sh
+++ to-get-taxa.sh
@@ -1,2 +1,3 @@
+python get-ncbi-id2.py resisGenes.nuc.fasta > ncbi-ids-for-taxon.txt
 bash id-to-taxid.sh ncbi-ids-for-taxon.txt > taxon-map.txt
 python taxon-to-text.py taxon-map.txt > taxon-list.txt
